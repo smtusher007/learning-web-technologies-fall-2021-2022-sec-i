a2971709b263bb3621f64e61acb8d578a992594d
tusher <smtusher007@gmail.com>
2021-10-17 15:46:31 +0600

    Lab Exam Added

---
+++ MID_LAB_EXAM/UserList.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>User List</title>
+</head>
+<body>
+
+	
+
+	<table border="1" align="center">
+		<tr>
+			<th>ID</th>
+			<th>NAME</th>
+			<th>USER TYPE</th>
+		</tr>
+		<tr>
+			<td>15-10101-1</td>
+			<td>Bob</td>
+			<td>Admin</td>
+		</tr>
+		<tr>
+			<td>16-10102-1</td>
+			<td>Anne</td>
+			<td>User</td>
+		</tr>
+		<tr>
+			<td>16-10103-2</td>
+			<td>Kent</td>
+			<td>Admin</td>
+		</tr>
+		<tr>
+			<td>16-10104-3</td>
+			<td>James</td>
+			<td>Admin</td>
+		</tr>
+		<tr>
+			<td colspan="3"><a href="home.php">Go home </a></td>
+		</tr>
+		
+		
+	
+
+	</table>
+</body>
+</html>
